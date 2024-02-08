7b32820a631daa3d58d724a4e49f2566b8371280
gabriel <dgabrielfof@gmail.com>
2024-02-08 16:17:41 -0300

    :sparkles: feat: show/hide password system

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  showPassword:boolean = false;
+  showPasswordIcon:string = "eye-closed"; //verificar o nome em src/assets/images/icons
+  passwordType:string = "password";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleShowPassword() {
+    this.showPassword = !this.showPassword;
+    this.passwordType = this.showPassword?"text":"password";
+    this.showPasswordIcon = this.showPassword?"eye-opened":"eye-closed"
+  }
 }
